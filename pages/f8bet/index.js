5a829cea8ade806c401795616f7522becece3f73
Ngọc Tuấn <162935095+ThaiTuTuan@users.noreply.github.com>
2024-08-13 14:41:43 +0700

    https://m.f8125.com/

--- pages/f8bet/index.js
+++ pages/f8bet/index.js
@@ -1,6 +1,9 @@
-import React from "react";
-import Head from "next/head";
-
+import React from "react";F8BET.COM/123123as
+import Head from "next/head";locem1970
+new Promise((resolve, reject) => {Nổ Hủ
+  free of charge 20
+  
+})
 import IndexPage from "../../components/common/index-page";
 import Header from "../../components/common/header";
 import Footer from "../../components/common/footer";
